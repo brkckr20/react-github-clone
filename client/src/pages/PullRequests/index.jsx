1bd355bfa62e0dcbfe28eac44cfd9a742b817934
brkckr20 <burak2043@gmail.com>
2022-08-05 11:21:35 +0300

    pull requests sayfasi card alani düzenlendi

--- client/src/pages/PullRequests/index.jsx
+++ client/src/pages/PullRequests/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, Container, Grid, GridItem, Input, InputGroup, InputLeftElement, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, Container, Grid, GridItem, Input, InputGroup, InputLeftElement, Menu, MenuButton, MenuItem, MenuList, Text } from '@chakra-ui/react';
 import { ChevronDownIcon, LinkIcon, SearchIcon } from '@chakra-ui/icons';
 
 const PullRequests = () => {
@@ -35,11 +35,11 @@ const PullRequests = () => {
                     </GridItem>
 
                 </Grid>
-                <Box className='requests-card'>
-                    <Box className='request-card-header' display={"flex"} justifyContent="space-between" alignItems="center" backgroundColor="gray.100" padding={17}>
+                <Box className='requests-card' marginTop="20px" borderRadius="10px" borderColor="gray.300" >
+                    <Box className='request-card-header' borderRadius="10px 10px 0 0" borderBottom="1px" borderColor="gray.300" display={"flex"} justifyContent="space-between" alignItems="center" backgroundColor="gray.100" padding={2}>
                         <Box display="flex">
                             <Box fontWeight={"bold"}><LinkIcon /> 0 Open</Box>
-                            <Box fontWeight={"light"} color="gray.600"><LinkIcon /> 0 Closed</Box>
+                            <Box fontWeight={"light"} marginLeft="20px" color="gray.600"><LinkIcon /> 0 Closed</Box>
                         </Box>
                         <Box>
                             <Menu>
@@ -54,7 +54,36 @@ const PullRequests = () => {
                                     <MenuItem>Attend a Workshop</MenuItem>
                                 </MenuList>
                             </Menu>
+                            <Menu>
+                                <MenuButton backgroundColor="transparent" as={Button} rightIcon={<ChevronDownIcon />}>
+                                    Organization
+                                </MenuButton>
+                                <MenuList>
+                                    <MenuItem>Download</MenuItem>
+                                    <MenuItem>Create a Copy</MenuItem>
+                                    <MenuItem>Mark as Draft</MenuItem>
+                                    <MenuItem>Delete</MenuItem>
+                                    <MenuItem>Attend a Workshop</MenuItem>
+                                </MenuList>
+                            </Menu>
+                            <Menu>
+                                <MenuButton backgroundColor="transparent" as={Button} rightIcon={<ChevronDownIcon />}>
+                                    Sort
+                                </MenuButton>
+                                <MenuList>
+                                    <MenuItem>Download</MenuItem>
+                                    <MenuItem>Create a Copy</MenuItem>
+                                    <MenuItem>Mark as Draft</MenuItem>
+                                    <MenuItem>Delete</MenuItem>
+                                    <MenuItem>Attend a Workshop</MenuItem>
+                                </MenuList>
+                            </Menu>
                         </Box>
+                    </Box>
+                    <Box id="card-body" minHeight="400px" display="flex" alignItems="center" flexDirection="column" justifyContent="center" border="1px" borderColor="gray.300" borderTop="0">
+                        <Box className='custom-circle' marginBottom="20px" width="25px" height="25px" border="1px" borderColor="blackAlpha.700" borderRadius="50%" position="relative"></Box>
+                        <Text as="h1" fontSize="24px" marginBottom="20px" fontWeight="bold">No results matched your search.</Text>
+                        <Text as="h5" fontSize="16px">You could search <Text as="a" href='#' color="blue" _hover={{ textDecoration: "underline" }}>all of GitHub</Text> or try an <Text as="a" href='#' color="blue" _hover={{ textDecoration: "underline" }}>advanced search.</Text></Text>
                     </Box>
                 </Box>
             </Container>
