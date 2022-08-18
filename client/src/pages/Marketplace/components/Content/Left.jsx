b1b42871b1702d025c96bf863a238c0a18bce03f
brkckr20 <burak2043@gmail.com>
2022-08-18 12:31:39 +0300

    marketplace ekranına devam edildi

--- client/src/pages/Marketplace/components/Content/Left.jsx
+++ client/src/pages/Marketplace/components/Content/Left.jsx
@@ -1,17 +1,47 @@
 import React from 'react'
-import { Box, ListItem, Text, UnorderedList } from '@chakra-ui/react'
-import { types } from './itemData'
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, ListItem, Text, UnorderedList } from '@chakra-ui/react'
+import { types, categories, filters } from './itemData'
 const Left = () => {
     return (
         <Box>
-            <Text as="p" fontSize="16px" letterSpacing={1}>Types</Text>
-            <UnorderedList listStyleType="none">
+            <Text as="p" fontSize="16px" letterSpacing={1} marginBottom="10px" fontWeight="bold">Types</Text>
+            <UnorderedList marginLeft="4px" listStyleType="none">
                 {
                     types.map((type, index) => (
-                        <ListItem _hover={{ color: "#3498db", cursor: "pointer" }} key={index} >{type.typeName}</ListItem>
+                        <ListItem _hover={{ color: "#3498db", cursor: "pointer" }} paddingTop="10px" paddingBottom="10px" key={index} >{type.typeName}</ListItem>
                     ))
                 }
             </UnorderedList>
+
+            <Text as="p" fontSize="16px" letterSpacing={1} marginBottom="10px" marginTop="10px" fontWeight="bold">Categories</Text>
+            <UnorderedList marginLeft="4px" listStyleType="none">
+                {
+                    categories.map((type, index) => (
+                        <ListItem _hover={{ color: "#3498db", cursor: "pointer" }} paddingTop="10px" paddingBottom="10px" key={index} >{type.name}</ListItem>
+                    ))
+                }
+            </UnorderedList>
+            <Accordion defaultIndex={[0]} allowMultiple>
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton padding={0} width="auto">
+                            <Box marginRight={20} marginTop="10px" textAlign='left' letterSpacing={1}>
+                                Filters
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel marginLeft="4px" padding={0} borderColor="transparent">
+                        <UnorderedList margin={0} listStyleType="none">
+                            {
+                                filters.map((type, index) => (
+                                    <ListItem _hover={{ color: "#3498db", cursor: "pointer" }} paddingTop="10px" paddingBottom="10px" key={index} >{type.name}</ListItem>
+                                ))
+                            }
+                        </UnorderedList>
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
         </Box>
     )
 }
--- client/src/pages/Marketplace/components/Content/itemData.js
+++ client/src/pages/Marketplace/components/Content/itemData.js
@@ -5,4 +5,80 @@ export const types = [
     {
         typeName: "Actions"
     }
-]
+];
+
+export const categories = [
+    {
+        name: "API management"
+    },
+    {
+        name: "Chat"
+    },
+    {
+        name: "Code quality"
+    },
+    {
+        name: "Code review"
+    },
+    {
+        name: "Continuous integration"
+    },
+    {
+        name: "Dependency management"
+    },
+    {
+        name: "Deployment"
+    },
+    {
+        name: "IDEs"
+    },
+    {
+        name: "Learning"
+    },
+    {
+        name: "Localization"
+    },
+    {
+        name: "Mobile"
+    },
+    {
+        name: "Monitoring"
+    },
+    {
+        name: "Project management"
+    },
+    {
+        name: "Publishing"
+    },
+    {
+        name: "Recently added"
+    },
+    {
+        name: "Securtiy"
+    },
+    {
+        name: "Support"
+    },
+    {
+        name: "Testing"
+    },
+    {
+        name: "Utilities"
+    }
+
+];
+
+export const filters = [
+    {
+        name: "Free"
+    },
+    {
+        name: "Free Trials"
+    },
+    {
+        name: "GitHub Enterprise"
+    },
+    {
+        name: "Paid"
+    }
+];
