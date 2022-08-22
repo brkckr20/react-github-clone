51186a46b3a7ed51ac40d80efe22984b88a2f604
Burak Çakır <burak2043@gmail.com>
2022-08-22 14:37:39 +0300

    right content alanina baslandi

--- client/src/pages/Marketplace/components/Content/Left.jsx
+++ client/src/pages/Marketplace/components/Content/Left.jsx
@@ -42,6 +42,24 @@ const Left = () => {
                     </AccordionPanel>
                 </AccordionItem>
             </Accordion>
+            <Text as="p" fontSize="16px" letterSpacing={1} marginBottom="10px" marginTop="10px" fontWeight="bold">Verification</Text>
+            <UnorderedList marginLeft="4px" listStyleType="none">
+                <ListItem _hover={{ color: "#3498db", cursor: "pointer" }} paddingTop="10px" paddingBottom="10px">Verified Creator</ListItem>
+            </UnorderedList>
+            <Accordion defaultIndex={[0]} allowMultiple>
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton padding={0} width="auto">
+                            <Box marginRight={20} marginTop="10px" textAlign='left' letterSpacing={1}>
+                                Your Items
+                            </Box>
+                            <AccordionIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel marginLeft="4px" padding={0} borderColor="transparent">
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
         </Box>
     )
 }
--- client/src/pages/Marketplace/components/Content/Right.jsx
+++ client/src/pages/Marketplace/components/Content/Right.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Box, Button, Input, InputGroup, InputLeftElement, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import { BiSearchAlt2 } from 'react-icons/bi'
+import { MdKeyboardArrowDown } from 'react-icons/md'
 
 const Right = () => {
     return (
-        <div>Right</div>
+        <Box>
+            <Box display="flex">
+                <Box width="100%" className='market-input'>
+                    <InputGroup>
+                        <InputLeftElement
+                            pointerEvents='none'
+                            children={<BiSearchAlt2 color='gray.300' />}
+                        />
+                        <Input placeholder='Search for apps and actions' backgroundColor="white" />
+                    </InputGroup>
+                </Box>
+                <Box>
+                    <Menu>
+                        <MenuButton marginLeft="10px" as={Button} rightIcon={<MdKeyboardArrowDown />} border="1px" borderColor="gray.300" backgroundColor="whiteAlpha.700">
+                            Sort : Best Match
+                        </MenuButton>
+                        <MenuList>
+                            <MenuItem>Best Match</MenuItem>
+                            <MenuItem>Recently added</MenuItem>
+                            <MenuItem>Most installed/starred</MenuItem>
+                        </MenuList>
+                    </Menu>
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
