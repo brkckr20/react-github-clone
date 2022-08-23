3385701a5507aac056c22cfc1e6d36e2af8c7781
Burak Çakır <burak2043@gmail.com>
2022-08-23 11:17:04 +0300

    right content icerik alanina devam edildi

--- client/src/pages/Marketplace/components/Content/itemData.js
+++ client/src/pages/Marketplace/components/Content/itemData.js
@@ -82,3 +82,23 @@ export const filters = [
         name: "Paid"
     }
 ];
+
+
+export const apps = [
+    {
+        id: 1,
+        logo: "https://images.unsplash.com/photo-1659631222920-4e085f98a4e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
+        appName: "Rewind Backups for GitHub (Formerly BackHub)",
+        author: "By Backhub",
+        title: "Daily, automatic backups of your repos and metadata. Restore your backups with metadata in seconds + Sync to your S3 or Azure",
+        install: "Recommended"
+    },
+    {
+        id: 2,
+        logo: "https://images.unsplash.com/photo-1659631222920-4e085f98a4e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
+        appName: "Rewind Backups for GitHub (Formerly BackHub)",
+        author: "By Backhub 2",
+        title: "Daily, automatic backups of your repos and metadata. Restore your backups with metadata in seconds + Sync to your S3 or Azure",
+        install: "Recommended"
+    }
+]
