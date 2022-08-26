7c1d1e6984f8f08885ebd9b6d08703de472a7f8a
Burak Çakır <burak2043@gmail.com>
2022-08-26 13:02:05 +0300

    marketplace icerik kismi tamamlandi

--- client/src/pages/Marketplace/components/Content/itemData.js
+++ client/src/pages/Marketplace/components/Content/itemData.js
@@ -182,3 +182,80 @@ export const apps2 = [
     },
 
 ]
+
+
+export const actions1 = [
+    {
+        id: 1,
+        appName: "First interaction",
+        author: "By actions",
+        title: "Greet new contributors when they create their first issue or open their first pull request",
+        stars: 140
+    },
+    {
+        id: 2,
+        appName: "Upload a Build Artifact",
+        author: "By actions",
+        title: "Upload a build artifact that can be used by subsequent workflow steps",
+        stars: 1600
+    },
+    {
+        id: 3,
+        appName: "Setup Java JDK",
+        author: "By actions",
+        title: "Set up a specific version of the Java JDK and add the command-line tools to the PATH",
+        stars: 784
+    },
+    {
+        id: 4,
+        appName: "Close Stale Issues",
+        author: "By actions",
+        title: "Close issues and pull requests with no recent activity",
+        stars: 712
+    },
+    {
+        id: 5,
+        appName: "Setup Go environment",
+        author: "By actions",
+        title: "Setup a Go environment and add it to the PATH",
+        stars: 850
+    }
+]
+
+export const actions2 = [
+    {
+        id: 1,
+        appName: "Setup .NET Core SDK",
+        author: "By actions",
+        title: "Used to build and publish .NET source. Set up a specific version of the .NET and authentication to private NuGet repository",
+        stars: 501
+    },
+    {
+        id: 2,
+        appName: "Download a Build Artifact",
+        author: "By actions",
+        title: "Download a build artifact that was previously uploaded in the workflow by the upload-artifact action",
+        stars: 539
+    },
+    {
+        id: 3,
+        appName: "Setup Node.js environment",
+        author: "By actions",
+        title: "Setup a Node.js environment by adding problem matchers and optionally downloading and adding it to the PATH",
+        stars: 2200
+    },
+    {
+        id: 4,
+        appName: "Cache",
+        author: "By actions",
+        title: "Cache artifacts like dependencies and build outputs to improve workflow execution time",
+        stars: 3000
+    },
+    {
+        id: 5,
+        appName: "action-git-diff-suggestions",
+        author: "By getsentry",
+        title: "This GitHub Action will take the current git changes and apply them as GitHub code review suggestions",
+        stars: 11
+    }
+]
