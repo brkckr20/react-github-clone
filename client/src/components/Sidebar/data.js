47fb992362331936b0313318e3de95b9f24a04a6
Burak Çakır <burak2043@gmail.com>
2022-09-28 15:25:19 +0300

    sidebar re-edited

---
+++ client/src/components/Sidebar/data.js
@@ -0,0 +1,9 @@
+export const repoData = [
+    { title: "portfolio-source" },
+    { title: "react-github-clone" },
+    { title: "html-css-notlar" },
+    { title: "JS" },
+    { title: "patika-nodejs" },
+    { title: "patika-react" },
+    { title: "react-chat-app" },
+]
--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Box, Button, Input, Text, Image, Divider } from '@chakra-ui/react'
 import './Sidebar.css';
 import { useLocation } from 'react-router-dom';
+import { repoData } from './data'
 
 const Sidebar = () => {
 
@@ -19,51 +20,30 @@ const Sidebar = () => {
                                 alignItems="center"
                                 marginBottom="4px"
                             >
-                                <Text as="h4">Your repositories</Text>
-                                <Button size="sm" colorScheme="green" ><i className="fa-solid fa-display my-icon"></i>New</Button>
+                                <Text as="h4" fontWeight="semibold" fontSize="14px">Your repositories</Text>
+                                <Button size="sm" backgroundColor="#2da44e" fontSize="12px" color="white" padding="6px 12px" height="auto" _hover={{ backgroundColor: "#2c974b" }}><i className="fa-solid fa-display my-icon"></i>New</Button>
                             </Box>
                             <Box marginBottom="16px">
-                                <Input placeholder='Find a repository...' size="md" />
+                                <Input placeholder='Find a repository...' size="sm" borderRadius="5px" fontSize="14px" backgroundColor="gray.50" border="1px solid" borderColor="gray.300" />
                             </Box>
                             <div className='side-list'>
-
-                                <div className='side-list-item'>
-                                    <Image rounded="50%" width="20px" height="20px" src='https://avatars.githubusercontent.com/u/73242451?s=16&v=4' mr="5px" />
-                                    <span>brkckr20/patikanodejs</span>
-                                </div>
-                                <div className='side-list-item'>
-                                    <Image rounded="50%" width="20px" height="20px" src='https://avatars.githubusercontent.com/u/73242451?s=16&v=4' mr="5px" />
-                                    <span>brkckr20/patikanodejs</span>
-                                </div>
-                                <div className='side-list-item'>
-                                    <Image rounded="50%" width="20px" height="20px" src='https://avatars.githubusercontent.com/u/73242451?s=16&v=4' mr="5px" />
-                                    <span>brkckr20/patikanodejs</span>
-                                </div>
-                                <div className='side-list-item'>
-                                    <Image rounded="50%" width="20px" height="20px" src='https://avatars.githubusercontent.com/u/73242451?s=16&v=4' mr="5px" />
-                                    <span>brkckr20/patikanodejs</span>
-                                </div>
-                                <div className='side-list-item'>
-                                    <Image rounded="50%" width="20px" height="20px" src='https://avatars.githubusercontent.com/u/73242451?s=16&v=4' mr="5px" />
-                                    <span>brkckr20/patikanodejs</span>
-                                </div>
-                                <div className='side-list-item'>
-                                    <Image rounded="50%" width="20px" height="20px" src='https://avatars.githubusercontent.com/u/73242451?s=16&v=4' mr="5px" />
-                                    <span>brkckr20/patikanodejs</span>
-                                </div>
-                                <div className='side-list-item'>
-                                    <Image rounded="50%" width="20px" height="20px" src='https://avatars.githubusercontent.com/u/73242451?s=16&v=4' mr="5px" />
-                                    <span>brkckr20/patikanodejs</span>
-                                </div>
+                                {
+                                    repoData.map((item, i) => (
+                                        <div className='side-list-item' key={i}>
+                                            <Image rounded="50%" width="20px" height="20px" src='https://avatars.githubusercontent.com/u/73242451?s=16&v=4' mr="5px" />
+                                            <span style={{ fontSize: "14px", marginBottom: "4px" }}>brkckr20/{item.title}</span>
+                                        </div>
+                                    ))
+                                }
                                 <Box paddingTop="25px" paddingBottom="25px">
-                                    <Text fontSize="sm" color="gray.500" transition="all" _hover={{ color: "blue" }} style={{ cursor: "pointer" }} transitionDuration={".3s"}>Show more</Text>
+                                    <Text fontSize="12px" color="gray.500" transition="all" _hover={{ color: "#0969da" }} style={{ cursor: "pointer" }} transitionDuration={".3s"}>Show more</Text>
                                 </Box>
                                 <Box>
-                                    <Divider />
+                                    <Divider backgroundColor="gray.300" />
                                 </Box>
-                                <Box paddingTop="25px">
-                                    <Text fontWeight="bold" marginBottom="10px">Recent activity</Text>
-                                    <Text as="p" fontSize="sm">
+                                <Box paddingTop="10px">
+                                    <Text fontWeight="semibold" fontSize="14px" marginBottom="10px">Recent activity</Text>
+                                    <Text as="p" fontSize="12px" color="gray.600">
                                         When you take actions across GitHub, we’ll provide links to that activity here.
                                     </Text>
                                 </Box>
